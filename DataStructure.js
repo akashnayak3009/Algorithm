667b7e5c6c7e63102cad40e05a4f37d5135f6328
akash <akashnayak3009@gmail.com>
2023-09-13 13:31:37 +0530

    array reversal

---
+++ DataStructure.js
@@ -0,0 +1,11 @@
+// //Array Reversal
+
+// function arrayReversal(arr){
+//     var temp=[]
+//     for(var i=0;i<arr.length;i++){
+//         temp[i]=arr[arr.length-1-i];
+//     }
+//     return temp;
+// }
+// const array=[5,4,9,8,7,3];
+// console.log(arrayReversal(array));
